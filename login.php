d3403544256d559119db7d3601765a16f554d857
Dabskiiee <2022310597@dhvsu.edu.ph>
2024-11-23 23:11:15 +0800

    Added input guests
    Added guest to complete the proposed log in reg

--- login.php
+++ login.php
@@ -20,6 +20,7 @@
           <input type="hidden" name="csrf_token" value="<?php echo $csrf_token?>">
           <input type="email" name="email" placeholder="Enter Email" class="curve" required><br>
           <input type="password" name="password" placeholder="Enter Password" class="curve" required><br>
+          <input type="number" name="guests" class="curve"><br>
           <p class="recover">
             <a href="forgot-password.php">Forgot Password?</a>
           <button type="submit" name="btn-signin">Sign in</button>
