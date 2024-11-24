34ac06a3c57202221f21b18cff924e952299d6e4
AlanJohn021 <2022310047@dhvsu.edu.ph>
2024-11-24 23:23:10 +0800

    javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,11 @@
-function showSection(sectionId) {
-    // Hide all sections
-    document.querySelectorAll('.wrapper').forEach(function(section) {
-      section.classList.remove('active');
-    });
-    // Show the clicked section
-    document.getElementById(sectionId).classList.add('active');
-  }
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
